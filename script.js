55ecc3925b2d8783e8a28e697feb70be0acd795f
MURATARISOY <arisoymurat@gmail.com>
2022-11-03 06:54:17 +0300

    the end

--- script.js
+++ script.js
@@ -26,6 +26,22 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
 
     });
+
+// typing animation script
+var typed = new Typed(".typing",{
+    strings:["Frond-End Developer","Designer","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2",{
+    strings:["Frond-End Developer","Designer","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
     // owl carousel script
     $('.carousel').owlCarousel({
         margin:20,
